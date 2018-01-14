93d9c430847f70d751585e9c823a453c7125f494
gohdaniel15 <gohdaniel15@gmail.com>
2018-01-14 21:58:40 +0800

    Add `contact-me` section

--- js/scroll.js
+++ js/scroll.js
@@ -1,13 +1,13 @@
 $(document).ready(function($) {
   $(window).scroll(function() {
-    if($(this).scrollTop() >= 600) {
-      $('section#code h1').addClass('color-code', 150);
-      $('section#code .title').addClass('color-title', 150);
-      $('section#code .description').addClass('color-description', 150);
+    if($(this).scrollTop() >= 500) {
+      $('section#code h1').addClass('color-code', 250);
+      $('section#code .title').addClass('color-title', 250);
+      $('section#code .description').addClass('color-description', 250);
     } else {
-      $('section#code h1').removeClass('color-code', 150);
-      $('section#code .title').removeClass('color-title', 150);
-      $('section#code .description').removeClass('color-description', 150);
+      $('section#code h1').removeClass('color-code', 250);
+      $('section#code .title').removeClass('color-title', 250);
+      $('section#code .description').removeClass('color-description', 250);
     }
   })
 });
